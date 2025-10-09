b37dc445f46a5125284b98bae484f05279363109
Alejandro llano <alejandrollano999@gmail.com>
2025-10-09 17:22:51 -0500

    Se agrego un menu al reto

--- Reinvencion del reto.py
+++ Reinvencion del reto.py
@@ -1,19 +1,13 @@
-def fuelsim():
-    # 1) Diccionario para la configuración
-    configuracion = {
+configuracion = {
         "initCG": 22.5,
         "uprLMT": 25,
         "lwrLMT": 20
     }
-    
-    # 2) Lista de tuplas para las opciones del menú
-    opciones_cg = [
+opciones_cg = [
         ("ALAS", -0.3),
         ("FUSELAJE CENTRAL", 0.5)
     ]
-
-    # 4) Diccionario para centralizar los mensajes (adaptado para f-strings)
-    mensajes = {
+mensajes = {
         "input_flt": "Ingrese la duración del vuelo (horas): ",
         "iteracion": "Iteración actual: ",
         "menu_opciones": "Seleccione la sección de tanques de combustible a consumir:\n 1. ALAS\n 2. FUSELAJE CENTRAL\n",
@@ -24,6 +18,15 @@ def fuelsim():
         "historial_cg": "Historial de CG durante el vuelo: ",
         "error_vuelo": "El tiempo de vuelo ingresado no es válido."
     }
+def fuelsim():
+    # 1) Diccionario para la configuración
+    
+    
+    # 2) Lista de tuplas para las opciones del menú
+    
+
+    # 4) Diccionario para centralizar los mensajes (adaptado para f-strings)
+    
 
     flt = int(input(mensajes["input_flt"]))
     currentCG = configuracion["initCG"]
@@ -54,12 +57,12 @@ def fuelsim():
         i += 1
 
     if configuracion["lwrLMT"] < currentCG < configuracion["uprLMT"] and 0 < flt < 20:
-       print(f'{mensajes["aterrizaje_seguro"]}{currentCG}')
+       print(f'{mensajes["aterrizaje_seguro"]}{currentCG:0.2f}')
        for a in cg_history:
         print(f'{mensajes["historial_cg"]}{a:0.2f}')
         #print(f'{mensajes["historial_cg"]}{cg_history}')
     elif currentCG < configuracion["lwrLMT"] or currentCG > configuracion["uprLMT"]:
-       print(f'{mensajes["aterrizaje_peligroso"]}{currentCG}')
+       print(f'{mensajes["aterrizaje_peligroso"]}{currentCG:0.2f}')
        for a in cg_history:
         print(f'{mensajes["historial_cg"]}{a:0.2f}') 
         #print(f'{mensajes["historial_cg"]}{cg_history}')
@@ -69,5 +72,28 @@ def fuelsim():
            
     return currentCG
 
-fuelsim()
+def diccionarios():
+    print("Diccionario de configuracion:")
+    for b, v in configuracion.items():
+        print(b,v)
+    print("Diccionario de mensajes")
+    for c, w in mensajes.items():
+        print(f"{c}: \t\t{w}")
 
+def listas():
+    for z,x in enumerate(opciones_cg):
+        print(f"{z+1}-{x[0]}: {x[1]}")
+
+while True:
+    choice = int(input("**¡Bienvenido!** Escoja una de las opciones a continuación:\n1. Simulador de Gestión del Centro de Gravedad\n2. Diccionarios\n3. listas\n4. Salir\n SU SELECCION: "))
+    match choice:
+        case 1:
+            fuelsim()
+        case 2:
+            diccionarios()
+        case 3:
+            listas()
+        case 4: 
+            break
+        case _:
+            print("valor ingresado invalido")
